311a28c34f6cd74f06bca606e86d0a92202315cf
alirezafazeli8 <alirezafazeli34@gmail.com>
2022-12-19 06:25:33 +0330

    new things i added in .txt file and i learned about number and float

--- values_variable/main.go
+++ values_variable/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	// // declare variable
 	// var greetingText string
@@ -24,17 +22,52 @@ func main() {
 
 	// fmt.Println(userNameTwo)
 
-	var userAge int = 18
+	// var userAge int = 18
 
-	userAgeTwo := 20
+	// userAgeTwo := 20
 
-	fmt.Println(userAge)
-	fmt.Println(userAgeTwo)
+	// fmt.Println(userAge)
+	// fmt.Println(userAgeTwo)
 
-	adminAge := userAge + 10
-	fmt.Println(adminAge)
+	// adminAge := userAge + 10
+	// fmt.Println(adminAge)
 
-	adminAge = 10 + 30
+	// adminAge = 10 + 30
 
-	fmt.Print(adminAge * 8)
+	// fmt.Print(adminAge * 8)
+
+	// var userName string =
+
+	// var userName string = "Alireza Fazeli"
+	// userName := "Alireza Fazeli"
+
+	// var userAge int = 18
+	// userAge := 18
+	// // userAge = 12.2 // cant
+	// // userAge = 12 // can
+	// // userAge = "sda" // cant
+	// fmt.Println(userAge)
+
+	// var score float64 = 12.33222
+
+	// fmt.Println(score)
+
+	// var numberOne int = 18
+	// var numberTwo float64 = 2.5
+
+	// var score float64 = float64(numberOne) / numberTwo // data should be convert with same variable type
+
+	// fmt.Println(score)
+
+	// userName := "Hi"
+	// userName = 12 // i cant do that like js or python
+
+	// // or
+
+	// userAge := 18
+	// userAge = 18.5 // i cant do that like js or python
+
+	// fmt.Println(userName)
+	// fmt.Println(userAge)
+
 }
